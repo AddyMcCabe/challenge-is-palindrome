f7d0f6f8459a0ee2a3cd0591a2c269526e4fc484
Addison McCabe <addisonhartmccabe@gmail.com>
2022-04-21 08:54:33 -0700

    final final commit

--- isPalindrome.js
+++ isPalindrome.js
@@ -4,10 +4,7 @@ function checkPalindrome(str){
     const reverseArrayValues = arrayValues.reverse();
     const reverseStr = reverseArrayValues.join('');
 
-    if(str === reverseStr){
-        console.log('It is a palindrome')
-    } else {
-        console.log('It is not a palindrome')
+    return str === reverseStr
+    
     }
-    }
-checkPalindrome('racecar')
+console.log(checkPalindrome('racecar'))
